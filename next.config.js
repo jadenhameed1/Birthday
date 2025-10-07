ef6d367440699671309f6e1776fcc82dab8670eb
jadenhameed1 <jadenhameed@gmail.com>
2025-10-07 17:33:13 +0000

    fix: remove static export to resolve build errors

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone', // or remove output completely
+  // Or use this for SSR instead of static export:
+  // trailingSlash: true,
+}
+
 module.exports = nextConfig
